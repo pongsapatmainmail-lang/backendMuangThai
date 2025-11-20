219cdec896ceff5836edcc245d119c8439242b13
unknown <pongsapat.mainmail@gmail.com>
2025-11-21 00:51:12 +0700

    create super user

--- products/apps.py
+++ products/apps.py
@@ -4,6 +4,3 @@ class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
 
-    def ready(self):
-        from .createsuperuser_on_startup import create_default_superuser
-        create_default_superuser()
